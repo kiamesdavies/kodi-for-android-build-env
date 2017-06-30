55d6ce5afc31a3d6caf6a70310180f780147362a
Akinniranye James <jamesniranye@gmail.com>
2017-07-01 00:04:17 +0100

    remove dangling

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+docker volume ls -qf dangling=true | xargs -r docker volume rm
+
 docker build -t kodi-andriod .
 
 docker run -it -v /home/encentral_ubuntu/Documents/kodi-andriod:/root/kodi-android  -w /root/kodi-android --name kodi-andriod kodi-adriod 
