86542453dfe8a062c3d106e6ee9d88cb4e9f7200
poseidon <jamesniranye@gmail.com>
2017-07-16 00:00:38 +0100

    change to relative path

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ docker volume ls -qf dangling=true | xargs -r docker volume rm
 
 docker build -t kodi-andriod .
 
-docker run -it -v /home/encentral_ubuntu/Documents/kodi-andriod:/root/kodi-android  -w /root/kodi-android --name kodi-andriod kodi-adriod 
+docker run -it -v $HOME/Documents/kodi-andriod:/root/kodi-android  -w /root/kodi-android --name kodi-andriod kodi-adriod 
